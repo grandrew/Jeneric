878699456a8d2f6d2099a3ea62f198d1ee508a02
Your Name <realgrandrew@gmail.com>
2010-08-03 19:48:54 +0400

    fixed compiler to fully compile right

--- ratbird/objj.js
+++ ratbird/objj.js
@@ -17,12 +17,10 @@ function traverse_path(where, path, last) {
   for(var i=0; i<(path.length-1); i++) {
     c = path[i];
     c.accessCount += 1;
-    //console.log("obname "+c.name);
     cur_obj = cur_obj[c.name];
     if(!cur_obj) return null; // object changed at runtime??
   }
-  //console.log("cur_obj  1 is " +cur_obj);
-  // now last part
+
   if((cur_obj instanceof Array) && (last.idx < cur_obj.length)) {
     last.name = last.idx;
     return { value: cur_obj[last.idx], name: last.idx, idx: 0, accessCount: 0};
@@ -46,39 +44,15 @@ function traverse_path_targ(where, path, last) {
   for(var i=0; i<(path.length-1); i++) {
     c = path[i];
     c.accessCount += 1;
-    //console.log("obname "+c.name);
-
-    if(!cur_obj[c.name]) {
-       //console.log("returning  null!!");
-       //return {value: {}, name: c.name, idx: 0, accessCount: 0};
-       return null; // object changed at runtime??
-    }
-
+    if(!cur_obj[c.name]) cur_obj[c.name] = {};
     cur_obj = cur_obj[c.name];
   }
-  //console.log("cur_obj  1 is " +cur_obj);
-  // now last part
-  /*
-  if(!cur_obj[last.name]) {
-    cur_obj[last.name] = {};
-    console.log("creating new!");
-  }
-  */
+
   if(typeof(last.name) === "number") cur_obj.length = last.name+1;
   if(!cur_obj[last.name]) return {value: cur_obj, name: last.name, idx: 0, accessCount: 0};
-  
+
   return {value: cur_obj[last.name], name: last.name, idx: 0, accessCount: 0};
 }
-
-/*
-function scanned_add(obj) {
-  for(var i=0; i<SCANBASE.length; i++) {
-    if(obj.value === SCANBASE[i]) return false;
-  }
-  SCANBASE.push(obj.value);
-  return true;
-}
-*/
 
 function find_step(PROC) {
     var path_desc = PROC.path_desc;
@@ -96,16 +70,13 @@ function find_step(PROC) {
     obj_desc.name = cur_obj.name;
     obj_desc.value = cur_obj.value;
 
-    // now add the value to current obj
-    // ... here
     if(cur_obj.value && cur_obj.value.exec && cur_obj.value.test && cur_obj.value.source) {
 	PROC.STOP = "Cannot compile inline regex's; consider using RegExp() prototype";
 	return;
     }
 
-
-    if(cur_obj.name === "tokenizer" || cur_obj.name === "lineno" || cur_obj.name === "filename" || cur_obj.name === "start" || cur_obj.name === "end") return;    
-    
+    if(cur_obj.name === "toSource" || cur_obj.name === "tokenizer" || cur_obj.name === "lineno" || cur_obj.name === "filename" || cur_obj.name === "start" || cur_obj.name === "end") return;    
+    if(typeof(cur_obj.value) === "function") return;
     if(typeof(cur_obj.value) !== "object") {
       if(typeof(cur_obj.value) !== "function")
           PROC.targ_current.value[cur_obj.name] = cur_obj.value;
@@ -116,35 +87,23 @@ function find_step(PROC) {
         PROC.targ_current.value[cur_obj.name] = null;
         return;
     }
- 
-    ///////
-    //if(cur_obj.name in NAME_DB) NAME_DB[cur_obj.name]++;
-    //else NAME_DB[cur_obj.name] = 1;
-    
 
     if(path_desc.length > _P_COMPILER.DEEP) {
       PROC.STOP = "Max stack DEEPness reached";
       return;
     }
-    PROC.targ_current.value[cur_obj.name] = {}
-    //if(typeof(cur_obj.name) === "number") PROC.targ_current.value["length"] = cur_obj.name+1;
-    //if(!scanned_add(cur_obj)) return;
     
     if ((cur_obj.value instanceof Array)) {
-      //if(!PROC.targ_current.value.hasOwnProperty("length")) PROC.targ_current.value.length = 0;
       path_desc.push({name: 0, idx: 0, value: cur_obj.value, accessCount: 0});
-//      PROC.targ_current.value[0] = {};
       return
     } else if(typeof(cur_obj.value) === "object") {
+    
       for(var ob in cur_obj.value) {
         path_desc.push({name: ob, idx: 0, value: cur_obj.value, accessCount: 0});
-//        PROC.targ_current.value[ob] = {};
         return
       }
     }  
 }
-
-//NAME_DB = {};
 
 objj = function _objj(parsed, onfinish, onerror) {
   var PROC = {};
@@ -153,8 +112,6 @@ objj = function _objj(parsed, onfinish, onerror) {
   PROC.path_desc = [{name: 0, value: parsed[0], idx:0}];
 
   PROC.STOP = false;
-  PROC.SCANNED = 0;
-//
   PROC._TS = (new Date()).getTime();
   var tt = (function travel_timer() {
       var TIMESLICE = 100;
@@ -162,27 +119,18 @@ objj = function _objj(parsed, onfinish, onerror) {
       while((PROC.path_desc.length) && ((new Date).getTime()) - ts0 < TIMESLICE) {
         find_step(PROC);
       }
-      //SCANNED += i;
       if(PROC.STOP) {
-        //console.log("STOP due to STOP=true");
 	onerror(PROC.STOP);
         return;
       }
 
       if(PROC.path_desc.length) setTimeout(arguments.callee, 50);
       else {
-        // finished. run onfinish
         onfinish(PROC.targ, PROC);
       }
   });
   setTimeout(tt, 50);
 }
-
-/*
-function sinfo() {
-  console.log("SCANNED: "+SCANNED+" SCANBASE: "+SCANBASE.length+" DEEP: "+path_desc.length+" reached:"+DEEPREACHED+" time: "+parseInt(((new Date()).getTime()-_TS)/1000)+"s Found: "+ FOUND.length);
-}
-*/
 
 })();
 
@@ -190,12 +138,7 @@ function test_objj() {
     var onf = function (res, proc) {
       OUT = res;
       PROC = proc;
-      /*
-        TODO: try to: 
-          1. JSON.stringify
-          2. JSON.parse
-          3. evaluate a VM with that code!
-      */
+
       console.log("Finished processing");
       var jj = JSON.parse(JSON.stringify(res));
       var vmss = new Jnaric();
